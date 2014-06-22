0cd37d32a0ef5e21b14a6eacce092e932b8985c9
mkawakami <mkawakami0304@gmail.com>
2014-06-22 10:31:33 +0900

    一通りの画面作成

--- src/com/mystica/shishunki/app/article/list/ArticleListActivity.java
+++ src/com/mystica/shishunki/app/article/list/ArticleListActivity.java
@@ -1,5 +1,6 @@
 package com.mystica.shishunki.app.article.list;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 
 import com.mystica.shishunki.R;
@@ -7,6 +8,10 @@ import com.mystica.shishunki.R;
 import android.support.v4.app.FragmentActivity;
 
 @EActivity(R.layout.activity_article_list)
-public class ArticleList extends FragmentActivity {
+public class ArticleListActivity extends FragmentActivity {
 
+	@AfterViews
+	protected void init() {
+		
+	}
 }
---
+++ src/com/mystica/shishunki/app/signin/SigninActivity.java
@@ -0,0 +1,54 @@
+package com.mystica.shishunki.app.signin;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.mystica.shishunki.R;
+import com.mystica.shishunki.app.article.list.ArticleListActivity_;
+import com.mystica.shishunki.app.signup.SignupActivity_;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+@EActivity(R.layout.activity_signin)
+public class SigninActivity extends FragmentActivity {
+	private static final String TAG = SigninActivity.class.toString();
+
+	@ViewById(R.id.editText1)
+	protected EditText etId;
+	@ViewById(R.id.editText2)
+	protected EditText etPassword;
+
+	@AfterViews
+	protected void init() {
+		Parse.initialize(this, "tb9z4W5lhHUuzwyyPTNGALb69KMuQEhrQQbBnxv7", "9azTcFYxpiIWC3vKVHGIoHedHOlWe1svF2zQf9Ob");
+		etId.setText("mkawakami0304@gmail.com");
+		etPassword.setText("password");
+	}
+
+	@Click(R.id.button1)
+	protected void login() {
+		ParseUser.logInInBackground(etId.getText().toString(), etPassword.getText().toString(), new LogInCallback() {
+			public void done(ParseUser user, ParseException e) {
+				if (user != null) {
+					Log.d(TAG, "ログイン成功");
+					ArticleListActivity_.intent(SigninActivity.this).start();
+				} else {
+					Log.e(TAG, "エラーによりログイン失敗");
+				}
+			}
+		});
+	}
+
+	@Click(R.id.button2)
+	protected void signup() {
+		SignupActivity_.intent(this).start();
+	}
+}
