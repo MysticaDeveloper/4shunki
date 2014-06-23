6b9b3f60499df12d76bc033e5a58ab55957accb1
hrfmsd <hrfmsd@gmail.com>
2014-06-23 17:50:54 +0900

    []広告導入＞ArticleListActivityに実装

--- src/com/mystica/shishunki/app/article/list/ArticleListActivity.java
+++ src/com/mystica/shishunki/app/article/list/ArticleListActivity.java
@@ -1,25 +1,28 @@
 package com.mystica.shishunki.app.article.list;
 
-import java.util.List;
-
-import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.Click;
-import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ItemClick;
-import org.androidannotations.annotations.ViewById;
-
 import android.support.v4.app.FragmentActivity;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 import com.mystica.shishunki.R;
 import com.mystica.shishunki.app.article.detail.ArticleDetailActivity_;
 import com.mystica.shishunki.dao.Article;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ItemClick;
+import org.androidannotations.annotations.ViewById;
+
+import java.util.List;
 
 @EActivity(R.layout.activity_article_list)
 public class ArticleListActivity extends FragmentActivity {
+
+	private AdView adView;
 
 	@ViewById(R.id.listView1)
 	protected ListView listViewArticle;
@@ -32,9 +35,47 @@ public class ArticleListActivity extends FragmentActivity {
 				listViewArticle.setAdapter(new ArticleListAdapter(instruments));
 			}
 		});
+
+		/**
+		 * AdMob表示
+		 */
+		adView = new AdView(this);
+		adView.setAdUnitId(getResources().getString(R.string.admob_unit_id));
+//		adView.setAdSize(AdSize.BANNER);
+		adView.setAdSize(AdSize.SMART_BANNER);
+
+		LinearLayout layout_ad_footer = (LinearLayout) findViewById(R.id.layout_ad_footer);
+		layout_ad_footer.addView(adView);
+
+		AdRequest adRequest = new AdRequest.Builder()
+				// all emulators
+				.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+				.addTestDevice(getResources().getString(R.string.debug_device_id_c1905))
+				.build();
+
+		adView.loadAd(adRequest);
 	}
+
 	@ItemClick(R.id.listView1)
 	protected void openDetail() {
 		ArticleDetailActivity_.intent(this).start();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		adView.resume();
+	}
+
+	@Override
+	protected void onPause() {
+		adView.pause();
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		adView.destroy();
+		super.onDestroy();
 	}
 }
